07a3d2de58f24256659b6354b32fe99cbf86262d
Shweta Agrawal <ag.shweta88@gmail.com>
2017-04-20 22:37:16 +0200

    Update and rename test.php to index.php

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Calculator</title>
+
+    <!-- Meta head content -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keyword" content="">
+
+    <!-- Favicon settings-->
+    <link rel="icon" href="img/favicon/favicon.ico">
+
+    <link rel="icon" type="image/png" sizes="192x192" href="img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+    <link rel="manifest" href="img/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Bootstrap switch css http://www.bootstrap-switch.org/ -->
+    <link href="css/bootstrap-switch.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <div id="home" class="intro">
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top navbar-right" id="navmain">
+            <div class="container">
+                <!-- Navbar toggle for mobile devices -->
+                <div class="navbar-header" id="mobilenav">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" id="burgerbutton">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand logo" data-target="#navbar" data-toggle="collapse">
+                        <img class="logo-img" src="" alt="UMAI" width="55" height="55">
+                    </a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a class="scroll" href="#about">About</a></li>
+                        <li><a class="scroll" href="#calculator">Calculator</a></li>
+                        <li><a class="scroll" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <div class="container">
+
+            <div class="content">
+                <h1 id="navchange">Calculator</h1>
+                <p class="lead">
+                    <br>
+                </p>
+                <p class="lead"><img class="img-square jumbo-logo" src="img/mustache-and-top-hat.png" alt="" width="128" height="128"></p>
+            </div>
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /.intro -->
+
+    <!-- About Section -->
+    <div id="about" class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">About me </h2>
+
+                </div>
+            </div>
+            <!-- About me -->
+
+            <div class="row section-content">
+                <div class="col-lg-2 col-lg-offset-2">
+                    <img class="img-circle" src="img/ava_512_1.jpg" alt="Generic placeholder image" width="140" height="140">
+                    <h3>Shweta Agrawal</h3>
+                    <p>Web/Application Developer</p>
+                    <div class="media-icons">
+                        <hr>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/shweta-agrawal-2a026791/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        </ul>
+                        <hr>
+                    </div>
+
+                </div>
+                <!-- /.col-lg-4 -->
+
+                <div class="col-lg-8">
+
+                    <p class="mb-0">
+                        I have done Master2 from Grenoble INP Ensimag, France where my specialization is “Advanced Information Systems and Software Engineering”. I spent 4 years as a Software developer in IT companies. Currently I am looking for job as a Web Developer or Project Manager. If I get the opportunity to be a part of that intellectually stimulating environment, I am sure my talent will be put to optimal use. I am confident that overall opportunity to work in your enterprise will broaden my horizons and give me an opportunity to gain world class environment and help me become an excellent world citizen. I can join immediately and I am sure that I will match all the credentials and will be able to maintain high standards at your organization.
+
+                    </p>
+                </div>
+                <!-- /.col-lg-4 -->
+
+            </div>
+            <!-- /.row -->
+
+            <!--          </div>-->
+            <!--        </div>-->
+        </div>
+    </div>
+    <!-- /About section -->
+
+    <!-- calculator Section -->
+    <div id="calculator" class="section invert">
+        <div class="container text-center" ng-app="calcApp">
+            <div class="section-title center">
+                <h2 class="section-heading">Interest calculator</h2>
+
+                <div class="space">
+                    <label class="control-label">Select Option:</label>
+                    <div class="">
+                        <select name="type" ng-model="interest.type" ng-dropdown required>
+
+                            <option ng-option value="Simple">Simple Interest</option>
+                            <option ng-option value="Compound">Compound Interest</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+
+                    <form>
+                        <div id="simple" ng-if="interest.type == 'Simple'">
+                            <div class="form-group" ng-controller="SimpleInterestCalculator" col-md-8>
+                                <label for="principal">Loan Amount:</label>
+                                <input type="text" id="principal" class="form-control" ng-model="principal" valid-number />
+                                <label for="months">No. of Months:</label>
+                                <input type="text" id="months" class="form-control" ng-model="months" valid-number />
+                                <label for="rate">Rate of Interest:</label>
+                                <input type="text" id="rate" class="form-control" ng-model="rate" valid-number />
+                                <br />
+                                <input type="button" value="Calculate" ng-click="Calculate()" />
+                                <br />
+                                <br />
+                                <div ng-hide="IsHidden">
+                                    <p class="bg-success">Total Amount: <strong>{{total | currency}}</strong></p>
+                                    <p class="bg-danger">Total Interest: <strong>{{interest | currency}}</strong></p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div id="compound" ng-if="interest.type == 'Compound'">
+
+                            <div class="form-group" ng-controller="CompoundInterestCalculator" col-md-8>
+                                <label for="principal">Loan Amount:</label>
+                                <input type="text" class="form-control" id="principal" ng-model="principal" valid-number />
+                                <label for="months">No. of Months:</label>
+                                <input type="text" class="form-control" id="months" ng-model="months" valid-number />
+                                <label for="rate">Rate of Interest:</label>
+                                <input type="text" class="form-control" id="rate" ng-model="rate" valid-number />
+                                <label for="frequency">Compound Frequency:</label>
+
+                                <select name="frequency" id="frequency" ng-model="frequency" ng-dropdown required class="form-control">
+
+                                    <option ng-option value="12">Yearly</option>
+                                    <option ng-option value="6">Half yearly</option>
+                                    <option ng-option value="4">Quarterly</option>
+
+                                </select>
+
+                                <br />
+                                <input type="button" value="Calculate" ng-click="Calculate()" />
+                                <br />
+                                <br />
+                                <div ng-hide="IsHidden">
+                                    <p class="bg-success">Total Amount: <strong>{{total | currency}}</strong></p>
+                                    <p class="bg-danger">Total Interest: <strong>{{interest | currency}}</strong></p>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Contact Section -->
+    <div id="contact" class="section">
+        <!-- <canvas id="cnv"></canvas>-->
+        <div class="container text-center">
+            <div class="section-title center">
+                <h2 class="section-heading">Drop me a line!</h2>
+                <div class="space"></div>
+            </div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <form name="sentMessage" id="contactForm" novalidate action="" method="post">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="email" id="email" class="form-control" placeholder="Email" required="required">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 messagebox">
+                        <div class="form-group">
+                            <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+
+                    <div id="success"></div>
+                    <button type="submit" class="btn btn-default">Send Message</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+    <div id="footer">
+        <div class="container text-center">
+            <div class="fnav">
+                <p>
+                    <br>
+
+                </p>
+            </div>
+
+            <!--    <div hidden>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>-->
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Angular Js-->
+    <script src="js/angular.min.js"></script>
+    <!-- interest calc js -->
+    <script src="js/interestCalculator.js"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery.1.11.1.js"><\/script>')
+    </script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-switch.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <script type="text/javascript" src="js/SmoothScroll.js"></script>
+    <script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+    <script type="text/javascript" src="js/contact_me.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+
+</html>
